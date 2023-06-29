c52eb19ec719b8ff846310ae83ae7328f2cf1eb7
NeyenFrandino <Neyenfrandino1@gmail.com>
2023-06-29 10:43:35 -0500

    Terminamos la web del cine con los requisitos solicitador, mas algunos extras de conocimiento que ya tenia fue aplicado

--- archivoJs.js
+++ archivoJs.js
@@ -31,7 +31,14 @@ function configuracionEscuchador(elementoID, parrafoBtnFocus){
             imgPelicula.classList.add('ImgPeliculaVolverAlFuturo')
             imgPelicula.style.display = 'inline-block';
 
+            let imgPelicula2 = document.getElementById('miImagen2')
+                imgPelicula2.classList.add('ImgPeliculaTheTrumanShow')
+                imgPelicula2.style.display = 'inline-block';
+
         } else {
+            let ImgDesaparece = buscarPorId('miImagen3');
+            ImgDesaparece.style.display = 'none'
+
             if(valorEdad >= 13){
                 let imgPelicula2 = document.getElementById('miImagen2')
                 imgPelicula2.classList.add('ImgPeliculaTheTrumanShow')
@@ -41,15 +48,28 @@ function configuracionEscuchador(elementoID, parrafoBtnFocus){
                 imgPelicula.classList.add('ImgPeliculaVolverAlFuturo')
                 imgPelicula.style.display = 'inline-block';
             } else {
+                let ImgDesaparece = buscarPorId('miImagen2');
+                ImgDesaparece.style.display = 'none'
+                
+                if(valorEdad >=7 ){
+                    let imgPelicula = document.getElementById('miImagen')
+                    imgPelicula.classList.add('ImgPeliculaVolverAlFuturo')
+                    imgPelicula.style.display = 'inline-block';
+            } else{
+                let ImgDesaparecee = buscarPorId('miImagen');
+                ImgDesaparecee.style.display = 'none'
+
                 let mensajeFinal = buscarPorId('mensajeFinal');
                 mensajeFinal.textContent = 'No hay peliculas disponibles para tu edad';
-                
+                setTimeout(function(){
+                    let mensajeFinal = buscarPorId('mensajeFinal');
+                    mensajeFinal.style.display ='none';
+                }, 5000)
+            
             }
-           
+          }
         }
-    })
-    
-    
+    });
  }
 
  function btnCrimen(){
@@ -68,14 +88,24 @@ function configuracionEscuchador(elementoID, parrafoBtnFocus){
             imgPelicula.style.display = 'inline-block';
 
         } else {
+            let ImgDesaparece = buscarPorId('miImagen5');
+            ImgDesaparece.style.display = 'none';
+
             if(valorEdad >= 13){
                 let imgPelicula2 = document.getElementById('miImagen4')
                 imgPelicula2.classList.add('ImgPeliculaElSecretoDeSusOjos')
                 imgPelicula2.style.display = 'inline-block';
 
             } else {
+                let ImgDesaparece = buscarPorId('miImagen2');
+                ImgDesaparece.style.display = 'none'
+
                 let mensajeFinal = buscarPorId('mensajeFinal');
                 mensajeFinal.textContent = 'No hay peliculas disponibles para tu edad';
+                setTimeout(function(){
+                    let mensajeFinal = buscarPorId('mensajeFinal');
+                    mensajeFinal.style.display ='none';
+                }, 5000)
                 
             }
            
@@ -84,12 +114,133 @@ function configuracionEscuchador(elementoID, parrafoBtnFocus){
     
     
  }
- 
+
+ function btnDrama(){
+    let btn = buscarPorId('botoDrama');
+    let edad = buscarPorId ('cajaEdad');
+    
+    btn.addEventListener('click', function(){
+        let valorEdad = edad.value;
+        if(valorEdad >= 16){
+            let imgPelicula1 = document.getElementById('miImagen8')
+            imgPelicula1.classList.add('ImgPeliculaTaxiDriver')
+            imgPelicula1.style.display = 'inline-block';
+
+            let imgPelicula2 = document.getElementById('miImagen7')
+            imgPelicula2.classList.add('ImgPeliculaTheShawshankRedemption')
+            imgPelicula2.style.display = 'inline-block';
+
+            let imgPelicula = document.getElementById('miImagen6')
+            imgPelicula.classList.add('ImgPeliculaCasBlancaATP')
+            imgPelicula.style.display = 'inline-block';
+
+        } else {
+            let ImgDesaparece = buscarPorId('miImagen8');
+            ImgDesaparece.style.display = 'none';
+
+            if(valorEdad >= 13){
+                let imgPelicula2 = document.getElementById('miImagen7')
+                imgPelicula2.classList.add('ImgPeliculaTheShawshankRedemption')
+                imgPelicula2.style.display = 'inline-block';
+
+                let imgPelicula = document.getElementById('miImagen6')
+                imgPelicula.classList.add('ImgPeliculaCasBlancaATP')
+                imgPelicula.style.display = 'inline-block';
+
+            } else {
+                let ImgDesaparece = buscarPorId('miImagen7');
+                ImgDesaparece.style.display = 'none'
+
+                if(valorEdad >=7 ){
+                    let imgPelicula = document.getElementById('miImagen6')
+                    imgPelicula.classList.add('ImgPeliculaVolverAlFuturo')
+                    imgPelicula.style.display = 'inline-block';
+            } else{
+                    let ImgDesaparecee = buscarPorId('miImagen6');
+                    ImgDesaparecee.style.display = 'none'
+
+                    let mensajeFinal = buscarPorId('mensajeFinal');
+                    mensajeFinal.textContent = 'No hay peliculas disponibles para tu edad';
+                    setTimeout(function(){
+                        let mensajeFinal = buscarPorId('mensajeFinal');
+                        mensajeFinal.style.display ='none';
+                    }, 5000)
+                }   
+            } 
+        }
+    })
+    
+    
+ }
+
+
+ function btnMusical(){
+    let btn = buscarPorId('botonMusical');
+    let edad = buscarPorId ('cajaEdad');
+    
+    btn.addEventListener('click', function(){
+        let valorEdad = edad.value;
+        if(valorEdad >= 16){
+            let imgPelicula1 = document.getElementById('miImagen11')
+            imgPelicula1.classList.add('ImgPeliculaTheRockyHorrorPictureShow')
+            imgPelicula1.style.display = 'inline-block';
+
+            let imgPelicula2 = document.getElementById('miImagen10')
+            imgPelicula2.classList.add('ImgPeliculaLesMiserables')
+            imgPelicula2.style.display = 'inline-block';
+
+            let imgPelicula = document.getElementById('miImagen9')
+            imgPelicula.classList.add('ImgPeliculaLaLaLand')
+            imgPelicula.style.display = 'inline-block';
+
+        } else {
+            let ImgDesaparece = buscarPorId('miImagen11');
+            ImgDesaparece.style.display = 'none';
+
+            if(valorEdad >= 13){
+                let imgPelicula2 = document.getElementById('miImagen10')
+                imgPelicula2.classList.add('ImgPeliculaLesMiserables')
+                imgPelicula2.style.display = 'inline-block';
+
+                let imgPelicula = document.getElementById('miImagen9')
+                imgPelicula.classList.add('ImgPeliculaLaLaLand')
+                imgPelicula.style.display = 'inline-block';
+
+            } else {
+                let ImgDesaparece = buscarPorId('miImagen10');
+                ImgDesaparece.style.display = 'none'
+
+                if(valorEdad >=7 ){
+                    let imgPelicula = document.getElementById('miImagen9')
+                    imgPelicula.classList.add('ImgPeliculaLaLaLand')
+                    imgPelicula.style.display = 'inline-block';
+            } else{
+                    let ImgDesaparecee = buscarPorId('miImagen9');
+                    ImgDesaparecee.style.display = 'none'
+
+                    let mensajeFinal = buscarPorId('mensajeFinal');
+                    mensajeFinal.textContent = 'No hay peliculas disponibles para tu edad';
+                    setTimeout(function(){
+                        let mensajeFinal = buscarPorId('mensajeFinal');
+                        mensajeFinal.style.display ='none';
+                    }, 5000)
+                }   
+            } 
+        }
+    })
+    
+    
+ }
+
+
+
+
+ btnMusical();
+ btnDrama();
  btnComedia();
- btnCrimen()
+ btnCrimen();
  configuracionEscuchador('cajaEdad', 'tituloSelectorEdad');
  configuracionEscuchador('cajaEdad', 'tituloSelectorEdad');
- configuracionEscuchador('cajaEdad', 'botonCrimen')
-
-
+ configuracionEscuchador('cajaEdad', 'botonCrimen');
+ configuracionEscuchador('cajaEdad', 'botonComedia');
 
