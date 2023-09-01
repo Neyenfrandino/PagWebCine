f5ae1228f1803aea8765444f28c6919a6c328e6d
NeyenFrandino <Neyenfrandino1@gmail.com>
2023-08-31 19:18:31 -0500

    avanzamos con la app

--- archivoJs.js
+++ archivoJs.js
@@ -1,5 +1,4 @@
 let datosJson;
-let categoriaSeleccionada = null;
 
 fetch('json.json')
 .then(res => res.json())
@@ -15,7 +14,76 @@ fetch('json.json')
 function buscarPorId(elementoID){
     let objetoId = document.getElementById(elementoID);
     return objetoId;
-} 
+}
+
+// Obtén una referencia al botón de búsqueda y agrega un oyente de eventos
+let boton = buscarPorId('miBotonDeBusqueda');
+boton.addEventListener('click', buscar);
+
+// Obtén una referencia al campo de entrada y a la lista donde se mostrarán los resultados
+let campoInput = buscarPorId('cajaEdad');
+let lista = buscarPorId('miLista');
+
+// Función para realizar la búsqueda y mostrar los resultados en la lista
+function buscar() {
+    // Limpia la lista antes de agregar nuevos elementos
+    lista.innerHTML = '';
+
+    // Realiza la solicitud fetch al archivo JSON
+    fetch('json.json')
+    .then(respuesta => respuesta.json())
+    .then(function(salida) {
+      let valorInput = campoInput.value.toUpperCase();
+      for (let categoria of salida.categorias) {
+        for (let tipo of categoria.tipos) {
+          for (let opcion of tipo.opciones) {
+            if (opcion.titulo.toUpperCase().startsWith(valorInput)) {
+              // Crea un elemento <p> para mostrar la sinopsis
+              let p = document.createElement('p');
+              p.innerHTML = opcion.sinopsis;
+              p.style.display ='block';
+              
+              // Crea un elemento <li> para mostrar el nombre
+              let li = document.createElement('li');
+              li.innerHTML = opcion.titulo;
+              li.addEventListener('mousemove', function(){
+              p.style.display = 'block';
+              });
+              li.addEventListener('mouseout', function(){
+                  p.style.display = 'none';
+              }); 
+
+              // Agrega el elemento <p> al <li> y el <li> a la lista
+              li.appendChild(p);
+              lista.appendChild(li);
+            }
+          }
+        }
+      }
+    });
+}
+
+function eventoPersonalizadoParrafo(parrafo) {
+    let originalContent = parrafo.innerHTML;
+      
+    parrafo.addEventListener('mouseover', function () {
+        let contenedor = parrafo.closest('.contenedor-opcion');
+        if (contenedor) {
+            parrafo.innerHTML = `
+              Tipo: ${parrafo.getAttribute('data-tipo')}<br>
+              Título: ${parrafo.getAttribute('data-titulo')}<br>
+              Sinopsis: ${parrafo.getAttribute('data-sinopsis')}
+            `;
+          }
+        });
+      
+    parrafo.addEventListener('mouseout', function () {
+          let contenedor = parrafo.closest('.contenedor-opcion');
+          if (contenedor) {
+            parrafo.innerHTML = originalContent;
+          }
+        });
+      }   
 
 function cambioSelect(){
     let nombreSelect = buscarPorId('selectSeriePeliculas');
@@ -24,16 +92,27 @@ function cambioSelect(){
     let botonDrama = buscarPorId('botoDrama');
     let botonCrecimientoPersonal = buscarPorId('botonCrecimientoPersonal');
     
+    
     nombreSelect.addEventListener('change', function(){
-        categoriaSeleccionada = datosJson.categorias.find(categoria => categoria.nombre === nombreSelect.value);
-        console.log(categoriaSeleccionada + 'este mensaje');
+        
+        let selectUsiario = buscarPorId('select');
+        let h3TituloUsuario = document.createElement('h3');
+        h3TituloUsuario.className = 'h3TituloUsuario'
+        h3TituloUsuario.textContent = nombreSelect.value;
+
+
+        selectUsiario.appendChild(h3TituloUsuario);
+
+        let categoriaSeleccionada = datosJson.categorias.find(categoria => categoria.nombre === nombreSelect.value);
+        console.log(nombreSelect.value + 'este mensaje');
         if(categoriaSeleccionada){
             btn.addEventListener('click', function(){
-                if(categoriaSeleccionada.nombre == 'Películas')
-                btnComedia('Películas')
-            else if(categoriaSeleccionada.nombre == 'Series'){
+                if(categoriaSeleccionada.nombre == 'Películas'){
+                    btnComedia('Películas');
+                   
+                }else if(categoriaSeleccionada.nombre == 'Series'){
                 // codigo de series...
-                btnComedia('Series')
+                btnComedia('Series');
                 }
             })
             botonTerror.addEventListener('click', function(){
@@ -55,17 +134,16 @@ function cambioSelect(){
                 if (categoriaSeleccionada.nombre === 'Películas') {
                     btnCrecimientoPersonal('Películas');
                 } else if (categoriaSeleccionada.nombre === 'Series') {
-                    btnTerror('Series');
+                    btnCrecimientoPersonal('Series');
                 }
             });
         };
     });
 };
-function btnComedia(categoriaSeleccUsuario) {
 
+function btnComedia(categoriaSeleccUsuario) {
     let div1 = buscarPorId('div1');
-    let divHijo = buscarPorId('divHijo');
-    div1.innerHTML = categoriaSeleccUsuario + ":\n";
+    div1.innerHTML = "";
 
     // Acceder a las categorías
     datosJson.categorias.forEach(categoria => {
@@ -73,10 +151,10 @@ function btnComedia(categoriaSeleccUsuario) {
             // Acceder a los tipos dentro de la categoría de películas
             categoria.tipos.forEach(tipo => {
                 if (tipo.nombre === 'Comedia') {
-                    // Acceder a las opciones dentro de la categoría de comedia
                     tipo.opciones.forEach(opcion => {
                         let contenedorOpcion = document.createElement('div');
                         contenedorOpcion.className = 'contenedor-opcion';
+                        contenedorOpcion.id = 'contenedor-opcion';
 
                         let imagenElemet = document.createElement('img');
                         imagenElemet.src = opcion.imagenes;
@@ -84,23 +162,18 @@ function btnComedia(categoriaSeleccUsuario) {
 
                         let textoElemet = document.createElement('p');
                         textoElemet.className = 'parrafoLista';
-                        textoElemet.textContent = `Tipo: ${tipo.nombre},
-                                                  Título: ${opcion.titulo},\n
-                                                  Sinopsis: ${opcion.sinopsis}`;
+                        textoElemet.textContent = `Mas informacion`;
 
                         contenedorOpcion.appendChild(imagenElemet);
                         contenedorOpcion.appendChild(textoElemet);
 
+                        // Agregar atributos de datos para eventoPersonalizadoParrafo
+                        textoElemet.setAttribute('data-tipo', tipo.nombre);
+                        textoElemet.setAttribute('data-titulo', opcion.titulo);
+                        textoElemet.setAttribute('data-sinopsis', opcion.sinopsis);
+
                         div1.appendChild(contenedorOpcion);
-
-                        contenedorOpcion.addEventListener('mouseover', function () {
-                            textoElemet.style.display = 'block';
-
-                        });
-
-                        contenedorOpcion.addEventListener('mouseout', function () {
-                            textoElemet.style.display = 'none';
-                        });
+                        eventoPersonalizadoParrafo(textoElemet);
                     });
                 }
             });
@@ -108,28 +181,39 @@ function btnComedia(categoriaSeleccUsuario) {
     });
 }
 
-
-
-function btnTerror(categoriaSeleccUsuario){
-    let elemetodiv1 = buscarPorId('div1');
-    elemetodiv1.innerHTML = categoriaSeleccUsuario +":\n";
+function btnTerror(categoriaSeleccUsuario) {
+    let div1 = buscarPorId('div1');
+    div1.innerHTML = "";
 
     // Acceder a las categorías
     datosJson.categorias.forEach(categoria => {
-        if (categoria.nombre === categoriaSeleccUsuario) { // Filtrar solo las películas
+        if (categoria.nombre === categoriaSeleccUsuario) {
             // Acceder a los tipos dentro de la categoría de películas
             categoria.tipos.forEach(tipo => {
-                if (tipo.nombre === 'Terror') { // Filtrar solo las películas de comedia
-                    // Acceder a las opciones dentro de la categoría de comedia
+                if (tipo.nombre === 'Terror') {
                     tipo.opciones.forEach(opcion => {
-                        console.log(`Título: ${opcion.titulo}`);
-                        console.log(`Sinopsis: ${opcion.sinopsis}`);
-                        let listItem = document.createElement('li');
-                        listItem.textContent = `Tipo: ${tipo.nombre},
-                                               Título: ${opcion.titulo}
-                                               Sinopsis: ${opcion.sinopsis}
-                                               Imagen: ${opcion.imagenes}`;
-                        elemetodiv1.appendChild(listItem);
+                        let contenedorOpcion = document.createElement('div');
+                        contenedorOpcion.className = 'contenedor-opcion';
+                        contenedorOpcion.id = 'contenedor-opcion';
+
+                        let imagenElemet = document.createElement('img');
+                        imagenElemet.src = opcion.imagenes;
+                        imagenElemet.className = 'ClaseImagenes';
+
+                        let textoElemet = document.createElement('p');
+                        textoElemet.className = 'parrafoLista';
+                        textoElemet.textContent = `Mas informacion`;
+
+                        contenedorOpcion.appendChild(imagenElemet);
+                        contenedorOpcion.appendChild(textoElemet);
+
+                        // Agregar atributos de datos para eventoPersonalizadoParrafo
+                        textoElemet.setAttribute('data-tipo', tipo.nombre);
+                        textoElemet.setAttribute('data-titulo', opcion.titulo);
+                        textoElemet.setAttribute('data-sinopsis', opcion.sinopsis);
+
+                        div1.appendChild(contenedorOpcion);
+                        eventoPersonalizadoParrafo(textoElemet);
                     });
                 }
             });
@@ -137,26 +221,39 @@ function btnTerror(categoriaSeleccUsuario){
     });
 }
 
-function btnDrama(categoriaSeleccUsuario){
-    let elemetodiv1 = buscarPorId('div1');
-    elemetodiv1.innerHTML = categoriaSeleccUsuario +":\n";
+function btnCrecimientoPersonal(categoriaSeleccUsuario) {
+    let div1 = buscarPorId('div1');
+    div1.innerHTML = "";
 
     // Acceder a las categorías
     datosJson.categorias.forEach(categoria => {
-        if (categoria.nombre === categoriaSeleccUsuario) { // Filtrar solo las películas
+        if (categoria.nombre === categoriaSeleccUsuario) {
             // Acceder a los tipos dentro de la categoría de películas
             categoria.tipos.forEach(tipo => {
-                if (tipo.nombre === 'Drama') { // Filtrar solo las películas de comedia
-                    // Acceder a las opciones dentro de la categoría de comedia
+                if (tipo.nombre === 'Crecimiento personal') {
                     tipo.opciones.forEach(opcion => {
-                        console.log(`Título: ${opcion.titulo}`);
-                        console.log(`Sinopsis: ${opcion.sinopsis}`);
-                        let listItem = document.createElement('li');
-                        listItem.textContent = `Tipo: ${tipo.nombre},
-                                               Título: ${opcion.titulo}
-                                               Sinopsis: ${opcion.sinopsis}
-                                               Imagen: ${opcion.imagenes}`;
-                        elemetodiv1.appendChild(listItem);
+                        let contenedorOpcion = document.createElement('div');
+                        contenedorOpcion.className = 'contenedor-opcion';
+                        contenedorOpcion.id = 'contenedor-opcion';
+
+                        let imagenElemet = document.createElement('img');
+                        imagenElemet.src = opcion.imagenes;
+                        imagenElemet.className = 'ClaseImagenes';
+
+                        let textoElemet = document.createElement('p');
+                        textoElemet.className = 'parrafoLista';
+                        textoElemet.textContent = `Mas informacion`;
+
+                        contenedorOpcion.appendChild(imagenElemet);
+                        contenedorOpcion.appendChild(textoElemet);
+
+                        // Agregar atributos de datos para eventoPersonalizadoParrafo
+                        textoElemet.setAttribute('data-tipo', tipo.nombre);
+                        textoElemet.setAttribute('data-titulo', opcion.titulo);
+                        textoElemet.setAttribute('data-sinopsis', opcion.sinopsis);
+
+                        div1.appendChild(contenedorOpcion);
+                        eventoPersonalizadoParrafo(textoElemet);
                     });
                 }
             });
@@ -164,26 +261,39 @@ function btnDrama(categoriaSeleccUsuario){
     });
 }
 
-function btnCrecimientoPersonal(categoriaSeleccUsuario){
-    let elemetodiv1 = buscarPorId('div1');
-    elemetodiv1.innerHTML = categoriaSeleccUsuario +":\n";
+function btnDrama(categoriaSeleccUsuario) {
+    let div1 = buscarPorId('div1');
+    div1.innerHTML = "";
 
     // Acceder a las categorías
     datosJson.categorias.forEach(categoria => {
-        if (categoria.nombre === categoriaSeleccUsuario) { // Filtrar solo las películas
+        if (categoria.nombre === categoriaSeleccUsuario) {
             // Acceder a los tipos dentro de la categoría de películas
             categoria.tipos.forEach(tipo => {
-                if (tipo.nombre === 'Crecimiento personal') { // Filtrar solo las películas de comedia
-                    // Acceder a las opciones dentro de la categoría de comedia
+                if (tipo.nombre === 'Drama') {
                     tipo.opciones.forEach(opcion => {
-                        console.log(`Título: ${opcion.titulo}`);
-                        console.log(`Sinopsis: ${opcion.sinopsis}`);
-                        let listItem = document.createElement('li');
-                        listItem.textContent = `Tipo: ${tipo.nombre},
-                                               Título: ${opcion.titulo}
-                                               Sinopsis: ${opcion.sinopsis}
-                                               Imagen: ${opcion.imagenes}`;
-                        elemetodiv1.appendChild(listItem);
+                        let contenedorOpcion = document.createElement('div');
+                        contenedorOpcion.className = 'contenedor-opcion';
+                        contenedorOpcion.id = 'contenedor-opcion';
+
+                        let imagenElemet = document.createElement('img');
+                        imagenElemet.src = opcion.imagenes;
+                        imagenElemet.className = 'ClaseImagenes';
+
+                        let textoElemet = document.createElement('p');
+                        textoElemet.className = 'parrafoLista';
+                        textoElemet.textContent = `Mas informacion`;
+
+                        contenedorOpcion.appendChild(imagenElemet);
+                        contenedorOpcion.appendChild(textoElemet);
+
+                        // Agregar atributos de datos para eventoPersonalizadoParrafo
+                        textoElemet.setAttribute('data-tipo', tipo.nombre);
+                        textoElemet.setAttribute('data-titulo', opcion.titulo);
+                        textoElemet.setAttribute('data-sinopsis', opcion.sinopsis);
+
+                        div1.appendChild(contenedorOpcion);
+                        eventoPersonalizadoParrafo(textoElemet);
                     });
                 }
             });
@@ -191,3 +301,4 @@ function btnCrecimientoPersonal(categoriaSeleccUsuario){
     });
 }
 
+    
